bfe8e9766cc3dc6ad602c13d45740078d86ca666
Dr Haartman <noordin.ramadhan1@gmail.com>
2025-08-12 11:25:13 +0300

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,24 @@
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#Inserting 15 at the second position (index 1)
+my_list.insert(1, 15)
+
+#Extending the list
+my_list.extend([50, 60, 70])
+
+#Removing the last element
+my_list.pop()
+
+#Sorting the list in ascending order
+my_list.sort()
+
+#Find and print the index of 30
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
+
+print("Final list:", my_list)
